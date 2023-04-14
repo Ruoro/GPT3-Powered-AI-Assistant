5704d18a3d2c6128a15d233626a62a07b866c925
Johnn <33524526+Ruoro@users.noreply.github.com>
2023-04-14 02:43:18 -0400

    voice Gpt with error in chatcompletion

---
+++ voice.py
@@ -0,0 +1,77 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+# import gTTs
+from gtts import gTTS
+# Set up the OpenAI API key
+openai.api_key = process.env.OPENAIKEY
+
+# Set up the speech engine
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+        try:
+            return recognizer.recognize_google(audio)
+        except:
+            print("Could not catch that")
+
+def get_response(prompt):
+    response = openai.ChatCompletion.create(
+        model = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1000,
+        n = 1,
+        stop = None,
+        temperature = 0.6,
+    )
+    return response['choices'][0]['text']
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    while True:
+        # wait for user to say genius
+        print ("Say 'Genius' to start recording your question")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "genius":
+                    # Record the user's question
+                    filename = 'input.wav'
+                    print ("Recording your question")  
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit = None, timeout = None)
+                        with open(filename, 'wb') as f:
+                            f.write(audio.get_wav_data())
+                    
+                # transcribe (question) audio to text
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print (f"You said: {text}")
+
+                        # get response from OpenAI
+                        response = get_response(text)
+                        print(f"Genius says: {response}")
+                        tts = gTTS(response, lang='en')
+                        tts.save("sample.mp3")
+                        
+                        speak_text(response)
+            except Exception as e:
+                print('An error occurred: {}'.format(e))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
